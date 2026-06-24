49a325899c271c3c2ec7dd520b5a62ed0e3ef889
Roc Deng <rocdeng@gmail.com>
2026-06-25 07:06:26 +0800

    Add scanner smoke test

--- Sources/AppManApp/AppManApp.swift
+++ Sources/AppManApp/AppManApp.swift
@@ -1,7 +1,22 @@
+import AppManCore
 import SwiftUI
 
 @main
 struct AppManApp: App {
+    init() {
+        if CommandLine.arguments.contains("--smoke-scan") {
+            do {
+                let apps = try InstallSourceResolver()
+                    .resolveInstallSources(for: AppScanner().scanInstalledApps())
+                print("APP_MAN_SMOKE_SCAN_COUNT=\(apps.count)")
+                exit(0)
+            } catch {
+                fputs("APP_MAN_SMOKE_SCAN_ERROR=\(error)\n", stderr)
+                exit(1)
+            }
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             AppListView()
